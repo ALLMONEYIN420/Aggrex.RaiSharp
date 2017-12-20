95a9cffe764372b06d6b72aba93ef8b2ac88b369
Darcy Thomas <dthomas@specnotes.com>
2017-12-19 22:27:18 -0500

    connect to seed peer.

--- src/Aggrex.ConsensusProtocol/Aggrex.ConsensusProtocol/LocalNode.cs
+++ src/Aggrex.ConsensusProtocol/Aggrex.ConsensusProtocol/LocalNode.cs
@@ -27,7 +27,6 @@ namespace Aggrex.ConsensusProtocol
         private readonly IPeerTracker _peerTracker;
         private readonly ILogger<LocalNode> _logger;
         private readonly ClientSettings _clientSettings;
-        private readonly IPEndPoint _seedNodeEndPoint;
         private RemoteNode.Factory _remoteNodeFactory { get; set; }
 
         public LocalNode(
@@ -54,22 +53,25 @@ namespace Aggrex.ConsensusProtocol
             LocalAddress = new IPEndPoint(IPAddress.Parse(localIpAddressDiscoverer.GetLocalIpAddress()), port);
 
             _logger.LogInformation($"Started Listening on {LocalAddress.Address}:{LocalAddress.Port}");
-            Console.WriteLine();
         }
 
         public IPEndPoint LocalAddress { get; private set; }
 
         public void Start()
         {
-            if (_clientSettings.NodeType != "SeedNode")
-            {
-                Task.Delay(2000).Wait();
-                Task.Run(() => ConnectToPeer(_seedNodeEndPoint));
-            }
             Task.Run(async () =>
             {
                 await _uPnPPortForwarder.ForwardPortIfNatFound();
                 _networkListenerLoop.StartListeningForConnections();
+            });
+
+            Task.Run(() =>
+            {
+                foreach (var peer in _clientSettings.BlockChainNetSettings.SeedPeers)
+                {
+                    IPAddress[] addresslist = Dns.GetHostAddresses(peer);
+                    ConnectToPeer(new IPEndPoint(addresslist[0], _clientSettings.BlockChainNetSettings.Port));
+                }
             });
 
             Task.Run(() => RequestMorePeersLoop());
@@ -78,34 +80,34 @@ namespace Aggrex.ConsensusProtocol
 
         private void RequestMorePeersLoop()
         {
-            while (true)
-            {
-                if (_peerTracker.NeedsMoreConnectedPeers)
-                {
-                    foreach (var remoteNode in _peerTracker.GetConnectedPeers())
-                    {
-                        if (!remoteNode.QueueContainsMessageType<RequestPeerAddressesMessage>())
-                        {
-                            remoteNode.QueueMessage(new RequestPeerAddressesMessage());
-                        }
+            //while (true)
+            //{
+            //    if (_peerTracker.NeedsMoreConnectedPeers)
+            //    {
+            //        foreach (var remoteNode in _peerTracker.GetConnectedPeers())
+            //        {
+            //            if (!remoteNode.QueueContainsMessageType<RequestPeerAddressesMessage>())
+            //            {
+            //                remoteNode.QueueMessage(new RequestPeerAddressesMessage());
+            //            }
 
-                        if (!remoteNode.QueueContainsMessageType<RequestActiveNodeSetMessage>())
-                        {
-                            remoteNode.QueueMessage(new RequestActiveNodeSetMessage());
-                        }
-                    }
+            //            if (!remoteNode.QueueContainsMessageType<RequestActiveNodeSetMessage>())
+            //            {
+            //                remoteNode.QueueMessage(new RequestActiveNodeSetMessage());
+            //            }
+            //        }
 
-                    foreach (var endpoint in _peerTracker.GetNotConnectedEndPoints(Int32.MaxValue, 0))
-                    {
-                        Task.Run(() => ConnectToPeer(endpoint));
-                    }
+            //        foreach (var endpoint in _peerTracker.GetNotConnectedEndPoints(Int32.MaxValue, 0))
+            //        {
+            //            Task.Run(() => ConnectToPeer(endpoint));
+            //        }
 
-                    for (int i = 0; i < 50; i++)
-                    {
-                        Thread.Sleep(100);
-                    }
-                }
-            }
+            //        for (int i = 0; i < 50; i++)
+            //        {
+            //            Thread.Sleep(100);
+            //        }
+            //    }
+            //}
         }
 
         private void ConnectToPeer(IPEndPoint endpoint)

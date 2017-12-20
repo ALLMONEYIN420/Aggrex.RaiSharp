f96666fbb7d5a2aee77da0b4a140dfe002a5fdf1
Darcy Thomas <dthomas@specnotes.com>
2017-12-19 22:28:45 -0500

    settings to connect to seed.

--- src/Aggrex.Configuration/Aggrex.Configuration/BlockChainNetSettings.cs
+++ src/Aggrex.Configuration/Aggrex.Configuration/BlockChainNetSettings.cs
@@ -5,6 +5,7 @@ namespace Aggrex.Configuration
     public class BlockChainNetSettings
     {
         public string Net { get; set; }
+        public int Port { get; set; }
         public string[] SeedPeers { get; set; }
     }
 }

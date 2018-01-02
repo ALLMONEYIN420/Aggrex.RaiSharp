47fec871c6a4f44c20df9097399a6c9f7880c112
Darcy Thomas <dthomas@specnotes.com>
2018-01-01 19:11:32 -0500

    remove transation processor from old project

--- src/Aggrex.ConsensusProtocol/Aggrex.ConsensusProtocol/Transactions/TransactionType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Aggrex.ConsensusProtocol.Transactions
-{
-    public enum TransactionType
-    {
-        TransferTransaction = 0x00,
-        DeployContractTransaction = 0x01,
-        InvokeContractTransaction = 0x02
-    }
-}
--- src/Aggrex.Network/Operations/OperationExecuter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Aggrex.Network.Operations
-{
-    public interface IOperationExecuter<T> where T : BaseOperation
-    {
-        void ExecuteOperation(T operation, IRemoteNode remoteNode);
-    }
-}

100ba68c8ebdac99fb0a3d666ddce0c9a0f80f52
Darcy Thomas <dthomas@specnotes.com>
2018-01-12 22:47:49 -0500

    fix upnp port forwarding

--- src/Aggrex.Network/UPnPPortForwarder.cs
+++ src/Aggrex.Network/UPnPPortForwarder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -13,19 +14,24 @@ namespace Aggrex.Network
 
         public async Task ForwardPortIfNatFound()
         {
-            return;
-            //try
-            //{
-            //    var discoverer = new NatDiscoverer();
-            //    var cts = new CancellationTokenSource(5000);
-            //    var device = await discoverer.DiscoverDeviceAsync(PortMapper.Upnp, cts);
+            try
+            {
+                var discoverer = new NatDiscoverer();
+                var cts = new CancellationTokenSource(5000);
+                var device = await discoverer.DiscoverDeviceAsync(PortMapper.Upnp, cts);
 
-            //    await device.CreatePortMapAsync(new Mapping(Protocol.Tcp, NetworkSettings.DefaultServerPrivatePort, NetworkSettings.DefaultServerExternalPort, "Aggrex Port Mapping"));
-            //}
-            //catch (NatDeviceNotFoundException ex)
-            //{
-            //    // If there isn't a Nat Device, we don't need to use UPnP 
-            //}
+                var existingMapping = await device.GetSpecificMappingAsync(Protocol.Udp, 7075);
+                if (existingMapping != null)
+                {
+                    await device.DeletePortMapAsync(existingMapping);
+                }
+
+                await device.CreatePortMapAsync(new Mapping(Protocol.Udp, 7075, 7075, "RaiBlocks"));
+            }
+            catch (Exception ex)
+            {
+                // If there isn't a Nat Device, we don't need to use UPnP 
+            }
         }
     }
 }
